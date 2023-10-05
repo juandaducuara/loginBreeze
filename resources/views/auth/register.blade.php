635a147b9165afe0ea939ce537cb24fa7506f0f4
juandaducuara <jdducuara63@misena.edu.co>
2023-10-05 16:37:49 -0500

    Registro

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,11 +2,23 @@
     <form method="POST" action="{{ route('register') }}">
         @csrf
 
-        <!-- Name -->
+        <!-- Nombre -->
         <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
+            <x-input-label for="name" :value="__('Nombre')" />
+            <x-text-input id="name" class="block mt-1 w-full" type="text" name="nombre" :value="old('name')" required autofocus autocomplete="name" />
+            <x-input-error :messages="$errors->get('nombre')" class="mt-2" />
+        </div>
+        <!-- Apellido -->
+        <div>
+            <x-input-label for="name" :value="__('Apellido')" />
+            <x-text-input id="name" class="block mt-1 w-full" type="text" name="apellido" :value="old('name')" required autofocus autocomplete="name" />
+            <x-input-error :messages="$errors->get('apellido')" class="mt-2" />
+        </div>
+        <!-- User -->
+        <div>
+            <x-input-label for="name" :value="__('Usuario')" />
+            <x-text-input id="name" class="block mt-1 w-full" type="text" name="user" :value="old('name')" required autofocus autocomplete="name" />
+            <x-input-error :messages="$errors->get('user')" class="mt-2" />
         </div>
 
         <!-- Email Address -->
@@ -37,6 +49,18 @@
                             name="password_confirmation" required autocomplete="new-password" />
 
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+        </div>
+        <!-- Tipo usuario -->
+        <div>
+            <x-input-label for="name" :value="__('Tipo usuario')" />
+            <x-text-input id="name" class="block mt-1 w-full" type="text" name="tipo_usuario" :value="old('name')" required autofocus autocomplete="name" />
+            <x-input-error :messages="$errors->get('tipo_usuaro')" class="mt-2" />
+        </div>
+        <!-- Estado -->
+        <div>
+            <x-input-label for="name" :value="__('Estado')" />
+            <x-text-input id="name" class="block mt-1 w-full" type="text" name="estado" :value="old('name')" required autofocus autocomplete="name" />
+            <x-input-error :messages="$errors->get('estado')" class="mt-2" />
         </div>
 
         <div class="flex items-center justify-end mt-4">
